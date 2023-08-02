755b39a547a74a9626fa0ff1c07fca7337edb478
Andre Leonardo <andre.leonardocb@gmail.com>
2023-08-01 23:45:53 -0300

    adicionando breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\{CursoController};
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,14 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/cursos/create', [CursoController::class, 'create'])->name('cursos.create');
-Route::delete('/cursos/{id}', [CursoController::class, 'destroy'])->name('cursos.destroy');
-Route::put('/cursos/{id}', [CursoController::class, 'update'])->name('cursos.update');
-Route::get('/cursos/{id}/edit', [CursoController::class,'edit'])->name('cursos.edit');
-Route::get('/cursos/{id}', [CursoController::class, 'show'])->name('cursos.show');
-Route::post('/cursos', [CursoController::class, 'store'])->name('cursos.store');
-Route::get('/cursos', [CursoController::class, 'index'])->name('cursos.index');
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
